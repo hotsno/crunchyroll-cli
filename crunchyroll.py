4697c87f84fd7733971e53b3a29f2cd5ddc2de7b
hotsno <71658949+hotsno@users.noreply.github.com>
2021-05-09 16:08:54 -0700

    Hopefully cleaner

--- crunchyroll.py
+++ crunchyroll.py
@@ -2,58 +2,75 @@ import requests
 import json
 import os
 
-# Here we define our query as a multi-line string
-query = '''
-query ($search: String) { # Define which variables will be used in the query (id)
-  Page(page: 1, perPage: 10) {
-    media(search: $search, type: ANIME) {
-      id
-      title {
-        romaji
-        english
-        native
-      }
-      streamingEpisodes {
-        url
-        title
+
+def anilistCall(query, variables):
+    url = 'https://graphql.anilist.co'
+    response = requests.post(
+        url, json={'query': query, 'variables': variables})
+    return response.json()
+
+
+def printSpacing():
+    print("\n-----------------------------------\n")
+
+
+def numberChoice():
+    choice = input("Enter number: ")
+    printSpacing()
+    return choice
+
+
+def animeSearch():
+    variables = {}
+    printSpacing()
+    searchTerm = input("Search for an anime: ")
+    variables["search"] = searchTerm
+
+    query = '''
+    query ($search: String) {
+      Page(page: 1, perPage: 10) {
+        media(search: $search, type: ANIME) {
+          title {
+            romaji
+            english
+            native
+          }
+          streamingEpisodes {
+            url
+            title
+          }
+        }
       }
     }
-  }
-}
-'''
+    '''
+    results = anilistCall(query, variables)
+    printSpacing()
+    print("SEARCH RESULTS:")
+    i = 1
+    for anime in results["data"]["Page"]["media"]:
+        print(str(i) + ": " + anime["title"]["romaji"])
+        i += 1
+    printSpacing()
+    return results
 
-variables = {
-}
 
-url = 'https://graphql.anilist.co'
+def listEpisodes(searchResults, animeChoice):
+    i = 1
+    print("AVAILABLE EPISODES:")
+    for episode in searchResults["data"]["Page"]["media"][int(animeChoice) - 1]["streamingEpisodes"]:
+        print(str(i) + ": " + episode["title"])
+        i += 1
+    printSpacing()
 
-print("\n-----------------------------------\n")
-search = input("Search for an anime: ")
-variables["search"] = search
-print("\n-----------------------------------\n")
 
-# Make the HTTP Api request
-response = requests.post(url, json={'query': query, 'variables': variables})
+def playEpisode(searchResults, animeChoice, episodeChoice):
+    print("Please wait... (10-15 secs)")
+    os.system("mpv --slang=enUS " + searchResults["data"]["Page"]["media"][int(
+        animeChoice) - 1]["streamingEpisodes"][int(episodeChoice) - 1]["url"])
 
-print("SEARCH RESULTS:")
-i = 1
-for anime in response.json()["data"]["Page"]["media"]:
-  print(str(i) + ": " + anime["title"]["romaji"])
-  i += 1
 
-print("\n-----------------------------------\n")
-animeChoice = input("Enter number: ")
-print("\n-----------------------------------\n")
-
-i = 1
-print("AVAILABLE EPISODES:")
-for episode in response.json()["data"]["Page"]["media"][int(animeChoice) - 1]["streamingEpisodes"]:
-  print(str(i) + ": " + episode["title"])
-  i += 1
-
-print("\n-----------------------------------\n")
-episodeChoice = input("Enter number: ")
-print("\n-----------------------------------\n")
-print("Please wait... (10-15 secs)")
-
-os.system("mpv --slang=enUS " + response.json()["data"]["Page"]["media"][int(animeChoice) - 1]["streamingEpisodes"][int(episodeChoice) - 1]["url"])
+searchResults = animeSearch()
+animeChoice = numberChoice()
+listEpisodes(searchResults, animeChoice)
+episodeChoice = numberChoice()
+playEpisode(searchResults, animeChoice, episodeChoice)
